537c3c1a6f0ad9ce092a1968b978ab3c196ac3ca
mehdi moosavi <71676494+smehdim@users.noreply.github.com>
2024-09-28 16:58:13 +0330

    Add files via upload

---
+++ HW#3/Q5_codes/main.py
@@ -0,0 +1,20 @@
+
+import os
+import numpy as np
+import torch
+import argparse
+from runner import *
+
+
+models = ['lin']
+datasets = ['health']
+N_explain = 10
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', type=int, default=0)
+
+args = parser.parse_args()
+
+run_benchmark(models, datasets, args.seed, N_explain)
